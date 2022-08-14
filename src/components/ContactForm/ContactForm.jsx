3356e044c6ebf42d1b93ab8e2171ee97dcfda782
AnnaKhanina <khanina.anna85@gmail.com>
2022-08-14 15:09:37 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -38,6 +38,7 @@ export class ContactForm extends React.Component {
             type="text"
             name="name"
             value={name}
+            placeholder="Enter name"
             onChange={this.handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -51,6 +52,7 @@ export class ContactForm extends React.Component {
             type="tel"
             name="number"
             value={number}
+            placeholder="123-45-67"
             onChange={this.handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
